328657b21270090aa43bdf7be2f41974a2531e42
JezielHonorato <136933391+JezielHonorato@users.noreply.github.com>
2026-05-03 10:29:49 -0300

    att

--- vue-project/src/router/index.ts
+++ vue-project/src/router/index.ts
@@ -1,13 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/:pathMatch(.*)*',
-      redirect: '/',
-    },
-  ],
+  history: createWebHashHistory(),
+  routes: [],
 })
 
 export default router
