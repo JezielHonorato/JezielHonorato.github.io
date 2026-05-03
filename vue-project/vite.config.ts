e7dbd85209a1d8675df5d7ac3b243d6c4114e87c
JezielHonorato <136933391+JezielHonorato@users.noreply.github.com>
2026-05-03 10:32:47 -0300

    Update vite.config.ts

--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'JezielHonorato.github.io',
+  base: '/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
